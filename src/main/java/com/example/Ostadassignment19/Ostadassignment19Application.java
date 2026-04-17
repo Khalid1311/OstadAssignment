e486ef7455cc476bf2df1e393b4289e9a96ae744
Khalid1311 <khalid1311.bd@gmail.com>
2026-04-17 18:44:26 +0600

    Init

---
+++ src/main/java/com/example/Ostadassignment19/Ostadassignment19Application.java
@@ -0,0 +1,13 @@
+package com.example.Ostadassignment19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ostadassignment19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ostadassignment19Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ostadassignment19/Ostadassignment19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ostadassignment19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ostadassignment19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
